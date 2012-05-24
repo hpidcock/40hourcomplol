2ee60de6debb8ab11c273a664fd8509385fc71a1
haza55@gmail.com <haza55@gmail.com@4be518d7-d6d8-18c5-c6a9-99c64b7c783f>
2012-05-24 11:08:06 +0000

    Camera Movement Parallax

--- Steves Challenge/Assets/Scripts/Camera.cs
+++ Steves Challenge/Assets/Scripts/Camera.cs
@@ -7,9 +7,13 @@ public class Camera : MonoBehaviour
 
 	public float m_Fade = 1.0f;
 
+	private Vector3 m_StartPosition;
+
 	void Start()
 	{
 		m_Game = (GameLogic)FindSceneObjectsOfType(typeof(GameLogic))[0];
+
+		m_StartPosition = transform.position;
 
 		camera.far = 1000.0f;
 	}
@@ -22,6 +26,8 @@ public class Camera : MonoBehaviour
 
 	void Update()
 	{
+		Vector3 lastPos = transform.position;
+
 		Vector3 minPos = new Vector3(1e30f, 1e30f, 1e30f);
 		Vector3 maxPos = new Vector3(-1e30f, -1e30f, -1e30f);
 
@@ -35,6 +41,19 @@ public class Camera : MonoBehaviour
 			maxPos.x = Mathf.Max(maxPos.x, pos.x);
 			maxPos.y = Mathf.Max(maxPos.y, pos.y);
 			maxPos.z = Mathf.Max(maxPos.z, pos.z);
+
+			if (player.m_controlState == PlayerControlState.PCS_OBJECT &&
+				player.m_currentTelePlatform != null)
+			{
+				pos = player.m_currentTelePlatform.transform.position;
+				minPos.x = Mathf.Min(minPos.x, pos.x);
+				minPos.y = Mathf.Min(minPos.y, pos.y);
+				minPos.z = Mathf.Min(minPos.z, pos.z);
+
+				maxPos.x = Mathf.Max(maxPos.x, pos.x);
+				maxPos.y = Mathf.Max(maxPos.y, pos.y);
+				maxPos.z = Mathf.Max(maxPos.z, pos.z);
+			}
 		}
 
 		const float approach = 64.0f;
@@ -42,6 +61,47 @@ public class Camera : MonoBehaviour
 		transform.position = (transform.position * approach + (minPos + maxPos) / 2.0f) / (approach + 1.0f);
 		transform.position = new Vector3(transform.position.x, transform.position.y, -100.0f);
 
-		camera.orthographicSize = Mathf.Clamp((camera.orthographicSize * approach + (maxPos.x - minPos.x) / 2.0f) / (approach + 1.0f), 16.0f, 1e30f);
+		float targetSize = 0.0f;
+
+		if (maxPos.x - minPos.x > maxPos.y - minPos.y)
+		{
+			targetSize = (maxPos.x - minPos.x) / 2.0f;
+		}
+		else
+		{
+			targetSize = (maxPos.y - minPos.y) / 2.0f;
+		}
+		camera.orthographicSize = Mathf.Clamp((camera.orthographicSize * approach + targetSize) / (approach + 1.0f), 16.0f, 1e30f);
+
+		const string paralaxName = "paralax_";
+
+		Vector3 moveDelta = transform.position - lastPos;
+
+		GameObject[] objects = (GameObject[])FindSceneObjectsOfType(typeof(GameObject));
+		foreach(GameObject obj in objects)
+		{
+			string t = obj.tag.ToLower();
+			if (t.Length > paralaxName.Length &&
+				t.Substring(0, paralaxName.Length) == paralaxName)
+			{
+				int i = 0;
+				int.TryParse(t.Substring(paralaxName.Length), out i);
+
+				if (i < 0)
+				{
+					float k = i * -1 + 1;
+					k *= 0.5f;
+
+					obj.transform.position += new Vector3(moveDelta.x * k, moveDelta.y * k, 0.0f);
+				}
+				else if (i > 0)
+				{
+					float k = i + 1;
+					k *= 2.0f;
+
+					obj.transform.position += new Vector3(moveDelta.x / k, moveDelta.y / k, 0.0f);
+				}
+			}
+		}
 	}
 }
