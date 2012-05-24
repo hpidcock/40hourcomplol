df0249d26e2b11d291c93816c1eb936add6a39c1
haza55@gmail.com <haza55@gmail.com@4be518d7-d6d8-18c5-c6a9-99c64b7c783f>
2012-05-24 13:36:51 +0000

    Menu, Loading and Platform+Player Glue

---
+++ Steves Challenge/Assets/Scripts/LoadingCamera.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class LoadingCamera : MonoBehaviour
+{
+	public Color m_Glow;
+
+	private float m_ChangeTime;
+	private float m_FlipTime;
+	private Rect m_CurrentRune;
+	private float m_Fade;
+	private int m_State;
+
+	private Rect[] m_Runes = new Rect[] {new Rect(0.0f, 0.75f, 0.25f, 0.25f),
+		new Rect(0.25f, 0.75f, 0.25f, 0.25f),
+		new Rect(0.0f, 0.5f, 0.25f, 0.25f),
+		new Rect(0.25f, 0.5f, 0.25f, 0.25f),
+		new Rect(0.0f, 0.25f, 0.25f, 0.25f),
+		new Rect(0.25f, 0.25f, 0.25f, 0.25f)
+	};
+
+	void Start()
+	{
+		m_ChangeTime = Time.time + 5.0f;
+		m_FlipTime = Time.time + 0.35f;
+		m_CurrentRune = m_Runes[Random.Range(0, m_Runes.Length - 1)];
+
+		m_Fade = 0.0f;
+		m_State = 0;
+	}
+
+	void Update()
+	{
+		if (m_State == 0)
+		{
+			m_Fade += Time.deltaTime;
+			if (m_Fade >= 1.0f)
+			{
+				m_State = 1;
+				m_Fade = 1.0f;
+			}
+		}
+		else if (m_State == 2)
+		{
+			m_Fade -= Time.deltaTime;
+			if (m_Fade <= 0.0f)
+			{
+				Application.LoadLevel(LevelData.NextLevel);
+			}
+		}
+
+		if (m_FlipTime < Time.time)
+		{
+			m_FlipTime = Time.time + Random.Range(0.35f, 0.8f);
+
+			m_CurrentRune = m_Runes[Random.Range(0, m_Runes.Length - 1)];
+		}
+
+		if (m_ChangeTime < Time.time)
+		{
+			m_State = 2;
+		}
+	}
+
+	void OnGUI()
+	{
+		int w = Screen.width;
+		int h = Screen.height;
+
+		m_Glow.a = (Mathf.Sin(Time.time * 2.0f) * 0.125f + 0.75f) * m_Fade;
+
+		GUI.color = m_Glow;
+		GUI.DrawTextureWithTexCoords(new Rect(w / 2 - 128 / 2, h / 2 - 128 / 2, 128, 128), guiTexture.texture, new Rect(0.5f, 0.5f, 0.5f, 0.5f));
+		GUI.DrawTextureWithTexCoords(new Rect(w / 2 - 32 / 2, h / 2 - 32 / 2, 32, 32), guiTexture.texture, m_CurrentRune);
+	}
+}
---
+++ Steves Challenge/Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+	public Color m_Color;
+
+	private float m_Fade;
+	private int m_State;
+
+	void Start()
+	{
+		m_Fade = 0.0f;
+		m_State = 0;
+	}
+
+	void Update()
+	{
+		if (m_State == 0)
+		{
+			m_Fade += Time.deltaTime;
+			if (m_Fade >= 1.0f)
+			{
+				m_State = 1;
+				m_Fade = 1.0f;
+			}
+		}
+		else if (m_State == 2)
+		{
+			m_Fade -= Time.deltaTime;
+			if (m_Fade <= 0.0f)
+			{
+				Application.LoadLevel("Loading");
+			}
+		}
+		else if (m_State == 3)
+		{
+			m_Fade -= Time.deltaTime;
+			if (m_Fade <= 0.0f)
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		m_Color.a = m_Fade;
+		GUI.color = m_Color;
+
+		if(GUI.Button(new Rect(Screen.width / 2 - 32, Screen.height / 2 - 32, 64, 32), "Start"))
+		{
+			LevelData.NextLevel = "Section_Intro";
+			m_State = 2;
+		}
+
+		if (GUI.Button(new Rect(Screen.width / 2 - 32, Screen.height / 2 + 4, 64, 32), "Quit"))
+		{
+			m_State = 3;
+		}
+	}
+}
