530e2c7190a2cc14bacf1391a873ddd49f7b4563
danmietz <danmietz@4be518d7-d6d8-18c5-c6a9-99c64b7c783f>
2012-05-29 03:42:25 +0000

    saving password to tortoise lol

---
+++ Steves Challenge/Assets/Scripts/CrateLogic.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+// Copy-pasta from PlatformLogic because if PlatformLogic is just added as a component, Start() is called afterward - with this,
+// it overwrites the physics settings (has gravity, locking axes)
+
+public class CrateLogic : PlatformLogic {
+	
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+		rigidbody.useGravity = true;
+		rigidbody.mass = m_mass;
+		rigidbody.drag = m_drag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision a_collision)
+	{
+		switch (a_collision.collider.tag)
+		{
+			case "Button":
+				ButtonLogic logic = a_collision.gameObject.GetComponentInChildren<ButtonLogic>();
+				logic.m_activators.Add(GetInstanceID());
+				break;
+		}
+	}
+
+	void OnCollisionExit(Collision a_collision)
+	{
+		switch (a_collision.collider.tag)
+		{
+			case "Button":
+				ButtonLogic logic = a_collision.gameObject.GetComponentInChildren<ButtonLogic>();
+				logic.m_activators.Remove(GetInstanceID());
+				break;
+		}
+	}
+	
+	public override void Activate()
+	{
+		base.Activate();
+		rigidbody.useGravity = false;
+		Debug.Log("Gravity off"); 
+	}
+	
+	public override void Deactivate()
+	{
+		base.Deactivate();
+		rigidbody.useGravity = true;
+		Debug.Log("Gravity on");
+	}
+}
